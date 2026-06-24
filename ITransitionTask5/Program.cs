30741bd7940590a0ad45b1354ce9a99ec1478a75
sherbola007 <sherali@inha.edu>
2026-06-25 01:59:41 +0900

    add backend docker + fix cors

--- ITransitionTask5/Program.cs
+++ ITransitionTask5/Program.cs
@@ -14,15 +14,15 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy.WithOrigins(
-    "https://localhost:7154",
-    "http://localhost:5174",
-    "http://82.208.22.154:5008"  
-)
-            .AllowAnyHeader()
-            .AllowAnyMethod();
+            "https://localhost:7154",
+            "http://localhost:5174",
+            "http://82.208.22.154:5008",
+            "http://82.208.22.154"  
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
     });
 });
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
